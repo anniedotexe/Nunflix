a6c469604821e9df149a1930c34dda23677160e4
anniedotexe <anniewu2303@gmail.com>
2023-06-25 11:14:41 -0700

    UPDATE Learn More Link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 function Footer() {
     return (
         <div className='footer'>
-            <p className='footer_text'>Learn More about the #SaveWarriorNun campaign <a href="https://linktr.ee/savewarriornun">here</a></p>
+            <p className='footer_text'>Learn More about the #SaveWarriorNun campaign at <a href="https://www.warriornun.com/">warriornun.com</a></p>
             <p className='footer_text'>Made with ❤️ by <a href="https://github.com/anniedotexe/Nunflix">anniedotexe</a></p>
         </div>
     )
