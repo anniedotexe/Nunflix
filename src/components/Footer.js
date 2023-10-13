19e16fe271dee077b99d5ab260a862c1f7958f26
Annie Wu <anniewu2303@gmail.com>
2023-10-12 20:20:53 -0700

    UPDATE Footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,6 @@ function Footer() {
     return (
         <div className='footer'>
             <p className='footer_text'>Learn More about the #SaveWarriorNun campaign at <a href="https://www.warriornun.com/">warriornun.com</a></p>
-            <p className='footer_text'>Made with ❤️ by <a href="https://github.com/anniedotexe/Nunflix">anniedotexe</a></p>
         </div>
     )
 }
